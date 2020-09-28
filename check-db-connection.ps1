5fc671c4129ee0c2245a98b7fcaddeafc5e0a4ee
ThoTran1990 <trandev90@gmail.com>
2020-09-29 00:30:43 +0700

    initial

---
+++ check-db-connection.ps1
@@ -0,0 +1,14 @@
+﻿# --------------------------------------------------------
+# Author: Tho Tran
+# Created Date: 29 Sep 2020
+# Desc: Check sql connection and alert
+# --------------------------------------------------------
+
+# Send email alert via smtp
+function Send-Email($instance, $isNetworkIssue){
+    $Subject = "Unable to connect database instance {0}" -f $instance
+    $Body = "<strong>Unable to connect database instance "+$instance+"</strong> </br>"
+
+    If($isNetworkIssue -eq $true){
+    }
+}
